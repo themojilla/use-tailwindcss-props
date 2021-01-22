af05f7d54b8b5201731a30d0e06f6a5a3bb779ae
so99ynoodles <so99ynoodles@gmail.com>
2021-01-22 13:25:40 +0900

    Add example

---
+++ example/cra/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "tailwind-aria";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button textColor="red-600">hello</Button>
+    </div>
+  );
+}
+
+export default App;
